309ac0b3793f51210a35516b6914b2a722550450
Lee Hae Kyun <dlgprbs9441@gmail.com>
2019-08-04 02:41:55 +0900

    Draw polygon in 3D space

--- Assets/Scripts/FoldPaper.cs
+++ Assets/Scripts/FoldPaper.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Papers
 {
     public List<Paper> paperList;
 
     public Papers()
     {
+        Debug.Log("Papers()");
         paperList = new List<Paper>();
     }
     public void makePaper(Vector3[] vertices)
     {
+        Debug.Log("makePaper()");
         paperList.Add(new Paper(vertices));
     }
     public class Paper
     {
         public GameObject paper;
+
         public Paper(Vector3[] vertices)
         {
+            Debug.Log("Paper()");
             paper = new GameObject();
             paper.AddComponent<MeshFilter>();
             paper.AddComponent<MeshRenderer>();
@@ -31,37 +34,61 @@ public class Papers
             mr.material.color = new Color(255, 255, 255);
             mesh.vertices = vertices;
 
-            var tris = new int[6]
-            {
-                // lower left triangle
-                0, 2, 1,
-                // upper right triangle
-                2, 3, 1,
-            };
+            var vertices2D = new Vector2[vertices.Length];
 
+            var allEqual = new bool[] { true, true, true };
+            var first_element = new double[] { vertices[0].x, vertices[0].y, vertices[0].z };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 1; j < vertices.Length; j++)
+                {
+                    if (first_element[i] != vertices[j][i])
+                    {
+                        allEqual[i] = false;
+                        break;
+                    }
+                }
+            }
+            for(int i=0; i<3; i++)
+            {
+                if (allEqual[i] || i == 2)
+                {
+                    for(int j=0; j<vertices2D.Length; j++)
+                    {
+                        vertices2D[j] = new Vector2(vertices[j][(i + 1) % 3], vertices[j][(i + 2) % 3]);
+                        Debug.Log(vertices2D[j]);
+                    }
+                    break;
+                }
+            }
+
+            Triangulator tr = new Triangulator(vertices2D);
+            var tris = tr.Triangulate();
             mesh.triangles = tris;
 
-            var normals = new Vector3[4]
+            var normals = new Vector3[vertices.Length];
+            for (int i = 0; i < vertices.Length; i++)
             {
-                -Vector3.forward,
-                -Vector3.forward,
-                -Vector3.forward,
-                -Vector3.forward
-            };
+                normals[i] = -Vector3.forward;
+            }
             mesh.normals = normals;
 
-            var uv = new Vector2[4]
+            var uv = new Vector2[vertices.Length];
+            for(int i=0; i<vertices.Length; i++)
             {
-                new Vector2(0, 0),
-                new Vector2(1, 0),
-                new Vector2(0, 1),
-                new Vector2(1, 1)
-            };
+                uv[i] = new Vector2(0, 0);
+            }
             mesh.uv = uv;
+
+            //Debug.Log("start");
+            foreach (var e in tris)
+            {
+                Debug.Log(e);
+            }
         }
     }
 
-    
+
 }
 public class FoldPaper : MonoBehaviour
 {
@@ -70,13 +97,15 @@ public class FoldPaper : MonoBehaviour
 
     void Start()
     {
-        var vertices = new Vector3[4]
+        Debug.Log("Start()");
+        var vertices = new Vector3[]
         {
-            new Vector3(0, 0, 0),
-            new Vector3(9, 0, 0),
-            new Vector3(0, 5, 0),
-            new Vector3(9, 5, 0),
+            new Vector3(0,0,0),
+            new Vector3(0,0,5),
+            new Vector3(0,5,5),
+            new Vector3(0,5,0),
         };
+        
         Papers p = new Papers();
         p.makePaper(vertices);
         p.paperList[0].paper.transform.position = new Vector3(0, 0, 0);
