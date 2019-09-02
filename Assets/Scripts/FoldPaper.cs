e4db8a9212a1eadc85066d54d1b73ad7a46386c1
Lee Hae Kyun <dlgprbs9441@gmail.com>
2019-09-02 11:49:09 +0900

    Backup:

--- Assets/Scripts/FoldPaper.cs
+++ Assets/Scripts/FoldPaper.cs
@@ -28,7 +28,6 @@ public class Paper : MonoBehaviour
 
     //TODO : 겹치는 면에 대해서(예를들어 2면 이상을 같이 접을 경우의 분할을 생각해야합니다.)
 
-
     public List<Paper> Tracking(Paper fix, Paper dynamic)
     {//TODO : tracking의 검증 필요, 검증은 안해봤음 아직
         List<Paper> trackgroup = new List<Paper>();
@@ -190,13 +189,34 @@ public class Paper : MonoBehaviour
     }
     /* End Make One Paper */
 
+    public static bool in_paper(Paper P, Vector3 pos)
+    {
+        Debug.Log("before " + pos.ToString());
+        var equation = makeEquation.make_plane_equation(new List<Vector3>(){P.vertices[0], P.vertices[1], P.vertices[2]});
+        pos = new Vector3(pos.x, pos.y, -(equation[0] * pos.x + equation[1] * pos.y + equation[3]) / equation[2]);
+        Debug.Log("after "+ pos.ToString());
+        var triangles = P.GetComponent<MeshFilter>().mesh.triangles;
+        RaycastHit hit = new RaycastHit(); hit.point = pos;
+        for (int i=0; i<triangles.Length; i += 3)
+        {
+            if(makeEquation.in_triangle(pos, new List<Vector3>() { P.vertices[triangles[i]], P.vertices[triangles[i + 1]], P.vertices[triangles[i + 2]] }))
+            {
+                FoldPaper.makeSphere(hit, Color.blue, 1);
+                return true;
+            }
+
+        }
+        FoldPaper.makeSphere(hit, Color.red, 1);
+        return false;
+    }
+
 }
 
 public class FoldPaper : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject mainPaper;
-    GameObject makeSphere(RaycastHit hit, Color color, int sphereCount)
+    public static GameObject makeSphere(RaycastHit hit, Color color, int sphereCount)
     {
         /* Make one sphere with hit and Color */
         Transform objectHit = hit.transform;
@@ -239,304 +259,24 @@ public class FoldPaper : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
             var paperLayer = LayerMask.NameToLayer("PAPER");
-            Debug.Log(paperLayer);
             if (Physics.Raycast(ray, out hit, 100.0f, 1 << paperLayer))
             {
-                Debug.Log("IN");
                 if (hit.collider.name.Contains("Paper"))
                 {
+                    Debug.Log(Paper.in_paper(hit.collider.gameObject.GetComponent<Paper>(), hit.point));
                     Vector3 colPosition = hit.collider.gameObject.transform.position;
-                    GameObject sphere = makeSphere(hit, Color.red, 1);
+                    //GameObject sphere = makeSphere(hit, Color.red, 1);
                     GameObject g = hit.collider.gameObject;
-                    sphere.transform.parent = g.transform;
-                    Debug.Log(sphere.transform.localPosition);
-                    Debug.Log(sphere.transform.position);
-                    Debug.Log(g.name);
+                    //sphere.transform.parent = g.transform;
+                    //Debug.Log(sphere.transform.localPosition);
+                    //Debug.Log(sphere.transform.position);
+                    //Debug.Log(g.name);
                 }
                 /* End Click*/
             }
         }
 
-        //for (int tt = 0; tt < Paper.paperList.Count; tt++)
-        //{
-        //    for (int i = 0; i < Paper.paperList[tt].GetComponent<Paper>().foldline.Count; i++)
-        //    {
-        //        if (Paper.paperList[tt].GetComponent<Paper>().foldline[i] != null)
-        //        {
-        //            Debug.DrawLine(Paper.paperList[tt].GetComponent<Paper>().vertices[Paper.paperList[tt].GetComponent<Paper>().vertices.Count - 1],
-        //                Paper.paperList[tt].GetComponent<Paper>().foldline[i].vertices[Paper.paperList[tt].GetComponent<Paper>().foldline[i].vertices.Count - 1]);
-
-        //        }
-        //    }
-        //}
-
     }
 }
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-
-//public class Paper
-//{
-//    static int paperCount = 0;
-//    public GameObject paper;
-//    public List<Vector3> vertices; // 마지막 vertices는 무게중심
-//    public int zindex = 0;
-//    public List<string> foldline; //선번호마다 연결된 다른 papername 만약 연결안되면 null값 
-
-//    public bool Folding(Vector2 startpoint, Vector2 endpoint) //이 Vector2는 Mesh 상의 vector입니다. 월드좌표계 사용시 변환 필요
-//    {
-//        int startindex = -1;
-//        int endindex = -1;
-
-//        //TODO : x1,x2,y1,y2가 기존 점과 동일할 경우에는 새로 점을 만드는것이 아니게 예외처리 해야합니다.
-//        for (int i = 0; i < vertices.Count - 1; i++)
-//        {
-//            if (Vector2.Distance(vertices[i], startpoint)
-//                + Vector2.Distance(vertices[(i + 1) % (vertices.Count - 1)], startpoint)
-//                - Vector2.Distance(vertices[i], vertices[(i + 1) % (vertices.Count - 1)]) <= 0.00001f)
-//            {
-//                startindex = i;
-//            }
-//            if (Vector2.Distance(vertices[i], endpoint)
-//                + Vector2.Distance(vertices[(i + 1) % (vertices.Count - 1)], endpoint)
-//                - Vector2.Distance(vertices[i], vertices[(i + 1) % (vertices.Count - 1)]) <= 0.00001f)
-//            {
-//                endindex = i;
-//            }
-
-//        }
-
-//        if (startindex == -1 || endindex == -1)
-//        {
-//            Debug.LogError("start, endindex fault");
-//            return false; // 직선 상에서 분명 좌표를 안잡았음..
-//        }
-
-//        //오른쪽 페이퍼 만들기 시작
-//        List<Vector3> newvertices = new List<Vector3>();
-//        List<string> newfoldline = new List<string>();
-//        newvertices.Add(startpoint);
-//        newfoldline.Add(null);
-
-//        for (int t = startindex + 1; t - 1 != endindex; t++)
-//        {
-//            if (t == vertices.Count - 1) t = 0;
-//            newvertices.Add(vertices[t]);
-//            newfoldline.Add(foldline[t]);
-//        }
-//        newvertices.Add(endpoint);
-//        newfoldline.RemoveAt(newfoldline.Count - 1);
-
-//        Papers.makePaper(newvertices);
-//        //왼쪽 페이퍼 만들기 시작
-//        List<Vector3> new2vertices = new List<Vector3>();
-//        List<string> new2foldline = new List<string>();
-//        new2vertices.Add(endpoint);
-//        new2foldline.Add(null);
-
-//        for (int t = endindex + 1; t - 1 != startindex; t++)
-//        {
-//            if (t == vertices.Count - 1) t = 0;
-//            new2vertices.Add(vertices[t]);
-//            new2foldline.Add(foldline[t]);
-//        }
-//        new2vertices.Add(startpoint);
-//        new2foldline.RemoveAt(new2foldline.Count - 1);
-
-//        Papers.makePaper(new2vertices);
-
-//        Papers.paperList[Papers.paperList.Count - 2].foldline.Add(Papers.paperList[Papers.paperList.Count - 1].paper.name);
-//        Papers.paperList[Papers.paperList.Count - 1].foldline.Add(Papers.paperList[Papers.paperList.Count - 2].paper.name);
-
-//        //TODO : 연결관계 가져오기, 현재의 클래스 지우기
-
-
-//        return true;
-//    }
-
-//    public Paper(List<Vector3> vertices)
-//    {
-//        /* Make One Paper */
-//        Debug.Log("Paper()");
-//        paper = new GameObject
-//        {
-//            layer = LayerMask.NameToLayer("PAPER"),
-//            name = "Paper Split " + paperCount++
-//        };
-
-//        var mf = paper.AddComponent<MeshFilter>();
-//        var mr = paper.AddComponent<MeshRenderer>();
-//        var mc = paper.AddComponent<MeshCollider>();
-
-//        var mesh = new Mesh();
-
-//        foldline = new List<string>(); //접힌면 다 빈칸채우기
-//        for (int i = 0; i < vertices.Count; i++)
-//        {
-//            foldline.Add(null);
-//        }
-
-//        Vector3 centermess = Vector3.zero;
-//        for (int i = 0; i < vertices.Count; i++)
-//        {
-//            centermess += vertices[i];
-//        }
-//        centermess /= vertices.Count;
-//        vertices.Add(centermess);
-
-//        this.vertices = vertices;
-
-//        Vector2[] uv = new Vector2[vertices.Count];
-//        for (int i = 0; i < vertices.Count; i++)
-//        {
-//            uv[i].x = vertices[i].x;
-//            uv[i].y = vertices[i].y;
-//        }
-//        int[] triangle = new int[(vertices.Count - 1) * 3]; //무게중심은 제
-
-//        for (int i = 0; i < (vertices.Count - 1) * 3; i += 3)
-//        {
-//            triangle[i] = i / 3;
-//            triangle[i + 1] = (i / 3 + 1) % (vertices.Count - 1);
-//            triangle[i + 2] = vertices.Count - 1;
-
-//        }
-//        Debug.Log(triangle);
-
-
-//        mesh.vertices = vertices.ToArray();
-//        mesh.triangles = triangle;
-
-//        mf.mesh = mesh;
-//        mr.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-//        mr.material.shader = Shader.Find("Custom/Double-Sided-Shader");
-//        mesh.uv = uv;
-
-//        mc.sharedMesh = mesh;
-
-
-//    }
-//    /* End Make One Paper */
-
-//    ///* check position is almost in edge of Paper and return position is exactly in edge*/
-//    //public Vector3 in_edge(Vector3 pos)
-//    //{
-//    //    for (int i = 0; i < vertices.Count; i++)
-//    //    {
-//    //        if (Vector3.Distance(vertices[i], pos) + Vector3.Distance(vertices[(i + 1) % vertices.Count], pos) -
-//    //            Vector3.Distance(vertices[i], vertices[(i + 1) % vertices.Count]) >= 0.0001f)
-//    //        {
-
-//    //        }
-//    //    }
-//    //}
-//    ///* END check position is in Edge of Paper */
-
-//}
-//public class Papers
-//{
-//    public static List<Paper> paperList;
-
-//    public Papers(int width, int height)
-//    {
-//        Debug.Log("Papers()");
-//        paperList = new List<Paper>();
-
-//        /* init first paper */
-//        List<Vector3> vertices = new List<Vector3>
-//        {
-//            new Vector3(0,0,0),
-//            new Vector3(0,height,0),
-//            new Vector3(width,height,0),
-//            new Vector3(width,0,0),
-//        };
-
-//        makePaper(vertices);
-//        paperList[0].paper.transform.position = new Vector3(0, 0, 0);
-//        /* End init first paper */
-
-
-//    }
-//    public static void makePaper(List<Vector3> vertices)
-//    {
-//        Debug.Log("makePaper()");
-//        Paper paper = new Paper(vertices);
-//        paperList.Add(paper);
-
-//    }
-
-//    public static Paper findPaper(string name)
-//    {
-//        foreach (var p in paperList)
-//        {
-//            if (name == p.paper.name)
-//                return p;
-//        }
-//        return null;
-//    }
-
-//}
-//public class FoldPaper : MonoBehaviour
-//{
-//    // Start is called before the first frame update
-//    GameObject mainPaper;
-//    Papers p;
-
-//    void Start()
-//    {
-//        int width = 1, height = 1;
-//        p = new Papers(width, height);
-//        Papers.paperList[0].Folding(new Vector2(0, 0.3f), new Vector2(1, 0.3f));
-//    }
-
-//    // Update is called once per frame
-
-
-//    private int paperLayer;
-//    List<Vector3> clickedPos =  new List<Vector3>();
-
-//    GameObject makeSphere(RaycastHit hit, Color color, int sphereCount)
-//    {
-//        /* Make one sphere with hit and Color */
-//        Transform objectHit = hit.transform;
-//        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//        Destroy(sphere.GetComponent<SphereCollider>());
-//        sphere.name = "Sphere " + sphereCount;
-//        sphere.transform.position = hit.point;
-//        sphere.transform.localScale -= new Vector3(0.95f, 0.95f, 0.95f);
-//        sphere.GetComponent<MeshRenderer>().material.color = color;
-//        return sphere;
-//        /* End Make one sphere with hit and Color */
-//    }
-
-//    void Update()
-//    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            RaycastHit hit;
-//            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-//            paperLayer = LayerMask.NameToLayer("PAPER");
-//            if (Physics.Raycast(ray, out hit, 100.0f, 1 << paperLayer))
-//            {
-//                if (hit.collider.name.Contains("Paper"))
-//                {
-//                    Vector3 colPosition = hit.collider.gameObject.transform.position;
-//                    GameObject sphere = makeSphere(hit, Color.red, 1);
-//                    GameObject g = hit.collider.gameObject;
-//                    sphere.transform.parent = g.transform;
-//                    Debug.Log(sphere.transform.localPosition);
-//                }
-//                /* End Click*/
-//            }
-//        }
-
-//    }
-
-//}
-
-
-//// is_in : to all triangles
--- Assets/Scripts/makeEquation.cs
+++ Assets/Scripts/makeEquation.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class makeEquation : MonoBehaviour
 {
-    List<float> make_plane_equation(Vector3[] vertices)
+    public static List<float> make_plane_equation(List<Vector3> vertices)
     {
         float x1 = vertices[0].x; float y1 = vertices[0].y; float z1 = vertices[0].z;
         float x2 = vertices[1].x; float y2 = vertices[1].y; float z2 = vertices[1].z;
         float x3 = vertices[2].x; float y3 = vertices[2].y; float z3 = vertices[2].z;
 
-        float A = y1*(z2 - z3) + y2*(z3 - z1) + y3*(z1 - z2);
-        float B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3*(x1 - x2);
+        float A = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2);
+        float B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2);
         float C = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
-        float D = -(x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3*(y1 * z2 - y2 * z1));
+        float D = -(x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3 * (y1 * z2 - y2 * z1));
 
         var retList = new List<float>();
         retList.Add(A);
@@ -21,11 +21,11 @@ public class makeEquation : MonoBehaviour
         retList.Add(C);
         retList.Add(D);
 
-        Debug.Log("A : " + A + ", " + "B : " + B + ", "+ "C : " + C + ", "+ "D : " + D + ", ");
+        Debug.Log("Plane Equation Result:  A : " + A + ", " + "B : " + B + ", " + "C : " + C + ", " + "D : " + D + ", ");
         return retList;
     }
 
-    List<float> make_line_equation(Vector2[] vertices)
+    public static List<float> make_line_equation(Vector2[] vertices)
     {
         float x1 = vertices[0].x; float y1 = vertices[0].y;
         float x2 = vertices[1].x; float y2 = vertices[1].y;
@@ -42,25 +42,24 @@ public class makeEquation : MonoBehaviour
         Debug.Log("A : " + A + ", " + "B : " + B + ", " + "C : " + C + ", ");
         return retList;
     }
-    bool in_plane(List<float> equation, Vector3 vertex)
+    public static bool in_plane(List<float> equation, Vector3 vertex)
     {
         float A = equation[0]; float B = equation[1]; float C = equation[2]; float D = equation[3];
-        Debug.Log((A * vertex.x + B * vertex.y + C * vertex.z + D) == 0);
+        Debug.Log("Is It On The Plane? :  " + ((A * vertex.x + B * vertex.y + C * vertex.z + D) == 0));
         return (A * vertex.x + B * vertex.y + C * vertex.z + D) == 0;
     }
 
-    float in_line(List<float> equation, Vector2 vertex)
+    public static float in_line(List<float> equation, Vector2 vertex)
     {
         float A = equation[0]; float B = equation[1]; float C = equation[2];
         return A * vertex.x + B * vertex.y + C;
     }
 
-
-    void in_vertices(List<float> equation, Vector3[] vertices)
+    public static bool in_triangle2(List<float> equation, Vector3 dot, List<Vector3> vertices)
     {
         var vertices2D = new Vector2[3];
         var allEqual = new bool[] { true, true, true };
-        var first_element = new double[] { vertices[0].x, vertices[0].y, vertices[0].z };
+        var first_element = new float[] { vertices[0].x, vertices[0].y, vertices[0].z };
         for (int i = 0; i < 3; i++)
         {
             for (int j = 1; j < 3; j++)
@@ -80,7 +79,53 @@ public class makeEquation : MonoBehaviour
                 {
                     vertices2D[j] = new Vector2(vertices[j][(i + 1) % 3], vertices[j][(i + 2) % 3]);
                     Debug.Log(vertices2D[j]);
-                    
+
+                }
+                break;
+            }
+        }
+
+        var line1 = make_line_equation(new Vector2[] { vertices2D[0], vertices2D[1] });
+        var line2 = make_line_equation(new Vector2[] { vertices2D[1], vertices2D[2] });
+        var line3 = make_line_equation(new Vector2[] { vertices2D[2], vertices2D[0] });
+
+       
+        if (in_line(line1, vertices2D[2]) * in_line(line1, new Vector2(dot.x, dot.y)) >= 0 &&
+            in_line(line2, vertices2D[0]) * in_line(line2, new Vector2(dot.x, dot.y)) >= 0 &&
+            in_line(line3, vertices2D[1]) * in_line(line3, new Vector2(dot.x, dot.y)) >= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void in_vertices(List<float> equation, Vector3[] vertices)
+    {
+        var vertices2D = new Vector2[3];
+        var allEqual = new bool[] { true, true, true };
+        var first_element = new float[] { vertices[0].x, vertices[0].y, vertices[0].z };
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 1; j < 3; j++)
+            {
+                if (first_element[i] != vertices[j][i])
+                {
+                    allEqual[i] = false;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (allEqual[i] || i == 2)
+            {
+                for (int j = 0; j < vertices2D.Length; j++)
+                {
+                    vertices2D[j] = new Vector2(vertices[j][(i + 1) % 3], vertices[j][(i + 2) % 3]);
+                    Debug.Log(vertices2D[j]);
+
                 }
                 break;
             }
@@ -91,7 +136,7 @@ public class makeEquation : MonoBehaviour
         var line3 = make_line_equation(new Vector2[] { vertices2D[2], vertices2D[0] });
 
         float max_x = vertices2D[0].x, min_x = vertices2D[0].x, max_y = vertices2D[0].y, min_y = vertices2D[0].y;
-        for(int i=1; i<3; i++)
+        for (int i = 1; i < 3; i++)
         {
             if (vertices2D[i].x > max_x)
                 max_x = vertices2D[i].x;
@@ -103,7 +148,7 @@ public class makeEquation : MonoBehaviour
                 min_y = vertices2D[i].y;
         }
         int count = 0;
-        for(float i = min_x; i<max_x; i+= 0.5f)
+        for (float i = min_x; i < max_x; i += 0.5f)
         {
             for (float j = min_y; j < max_y; j += 0.5f)
             {
@@ -115,7 +160,7 @@ public class makeEquation : MonoBehaviour
                     if (allEqual[0])
                     {
                         point = new Vector3(0, i, j);
-                        float x = calc_plane_equation(equation, point)/equation[0];
+                        float x = calc_plane_equation(equation, point) / equation[0];
                         point.x = x;
 
                     }
@@ -131,7 +176,7 @@ public class makeEquation : MonoBehaviour
                         float z = calc_plane_equation(equation, point) / equation[2];
                         point.z = z;
                     }
-                    List<float> newEquation = make_plane_equation(new Vector3[] { vertices[0], vertices[1], point });
+                    //List<float> newEquation = make_plane_equation(new Vector3[] { vertices[0], vertices[1], point });
                     count++;
                 }
             }
@@ -140,29 +185,50 @@ public class makeEquation : MonoBehaviour
     }
     // Start is called before the first frame update
 
-    float calc_plane_equation(List<float> equation, Vector3 vertex)
+    public static float calc_plane_equation(List<float> equation, Vector3 vertex)
     {
         float A = equation[0]; float B = equation[1]; float C = equation[2]; float D = equation[3];
         return -(A * vertex.x + B * vertex.y + C * vertex.z + D);
         // ax + by + cz + d = 0
     }
+
+    public static bool in_triangle(Vector3 dot, List<Vector3> vertex)
+    {
+        Vector3 d1 = vertex[1] - vertex[0];
+        Vector3 d2 = vertex[2] - vertex[1];
+        Vector3 d3 = vertex[0] - vertex[2];
+
+        Vector3 p1 = dot - vertex[0];
+        Vector3 p2 = dot - vertex[1];
+        Vector3 p3 = dot - vertex[2];
+
+        if (
+            Vector3.Dot(Vector3.Cross(d1, p1), Vector3.Cross(p1, -d3)) >= 0 &&
+            Vector3.Dot(Vector3.Cross(d2, p2), Vector3.Cross(p2, -d1)) >= 0 &&
+            Vector3.Dot(Vector3.Cross(d3, p3), Vector3.Cross(p3, -d2)) >= 0
+            )
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    public static bool in_line_3d(Vector3 dot, List<Vector3> line)
+    {
+        Vector3 d = line[1] - line[0];
+        Vector3 p = dot - line[0];
+
+        return Vector3.Cross(d, p) == Vector3.zero;
+    }
+
     void Start()
     {
-        var vertices = new Vector3[]
-        {
-            new Vector3(1,-2,0),
-            new Vector3(3, 1, 4),
-            new Vector3(0,-1,2)
-        };
-        var equation = make_plane_equation(vertices);
-        in_plane(equation, new Vector3(1, 1, 1));
-        in_vertices(equation, vertices);
+
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
